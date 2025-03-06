127ef4402f93e4438812e91f8c330fea2706b80e
DeepakKumarSolanki <dksolanki790@gmail.com>
2025-03-06 18:26:32 +0530

    build-10

---
+++ build/build/build/build/build/build/controllers/payment/_routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const payment = require('./payment');
+const router = express.Router();
+router.post('/add', payment.add);
+router.get('/', payment.index);
+module.exports = router;
---
+++ build/build/build/build/build/controllers/status/_routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const auth = require('../../middelwares/auth');
+const status = require('./status');
+const router = express.Router();
+router.get('/', auth, status.index);
+module.exports = router;
