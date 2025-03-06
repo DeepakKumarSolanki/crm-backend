1a2a2721a4dc8a7deaf0a9e796dc0ba5ca0c0c3c
DeepakKumarSolanki <dksolanki790@gmail.com>
2025-03-06 18:06:03 +0530

    build-9

---
+++ build/build/build/controllers/route/_routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const route = require('./route');
+const auth = require('../../middelwares/auth');
+const router = express.Router();
+router.get('/', auth, route.index);
+module.exports = router;
